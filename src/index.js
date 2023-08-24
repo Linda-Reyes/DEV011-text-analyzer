2f41b759dcd303fc418bfbe280817afd1c62d64e
Linda Reyes <lindamreyesr@gmail.com>
2023-08-24 00:25:25 -0500

    Estructura en html,referencia desde JS del elemento textarea y escuchar el input del elemento

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textarea = document.querySelector('textarea[placeholder="Comienza a escribir un texto aqui..."]');
+
+//let WordCount = analyzer.getWordCount(textarea.value)
+textarea.addEventListener("input",(e)=>{
+    console.log(e.target.value) 
+})
