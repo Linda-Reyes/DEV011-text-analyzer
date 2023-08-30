cc54ca8d8058a4ad07206be680bb94106ebf6065
Linda Reyes <lindamreyesr@gmail.com>
2023-08-29 22:35:45 -0500

    CSS y algunas funciones de JS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,15 +1,28 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let wordsAmount= text.split(" ")
+    return wordsAmount.length
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let CharacterAmount= text.length // creo 
+    return CharacterAmount
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+   // Eliminamos los espacios y signos de puntuación utilizando una expresión regular
+   const cleanedText = text.replace(/[^\w\s]|_/g, '').replace(/\s+/g, '');
+
+   // Contamos los caracteres restantes
+   const characterCount = cleanedText.length;
+ 
+   // Retornamos el recuento de caracteres
+   return characterCount;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
@@ -17,6 +30,7 @@ const analyzer = {
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
+  
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 const textarea = document.querySelector('textarea[placeholder="Comienza a escribir un texto aqui..."]');
+const wordCountBadge = document.querySelector('span[data-testing="word-count-badge"]') /* Hago referencia al elemento de HTML, paramanipularlo desde JS*/
+const characterCountBadge = document.querySelector('span[data-testing="character-count-badge"]')
+const characterNoSpacesCountBadge = document.querySelector('span[data-testing="character-no-spaces-count-badge"]')
 
 //let WordCount = analyzer.getWordCount(textarea.value)
 textarea.addEventListener("input",(e)=>{
-    console.log(e.target.value) 
+    //console.log(e.target.value) 
+    let words = analyzer.getWordCount(textarea.value)
+    wordCountBadge.textContent= words;
+    let characters = analyzer.getCharacterCount(textarea.value)
+    characterCountBadge.textContent= characters;
+    let characterNoSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value)
+    characterNoSpacesCountBadge.textContent= characterNoSpaces
 })
+
