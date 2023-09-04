c15ce1ec370417ec32b3a7e08e6f5ef1f1645d9f
Linda Reyes <lindamreyesr@gmail.com>
2023-09-03 22:03:23 -0500

    Conteo de numeros y botton reset

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,34 +1,39 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    let wordsAmount= text.split(" ")
-    return wordsAmount.length
+    //Separamos las parablas teniendo en cuenta el espacio
+    const wordsCount= text.split(" ")
+    if (wordsCount[wordsCount.length - 1] == ''){
+      return wordsCount.length - 1
+    }else {
+      return wordsCount.length
+    }
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    let CharacterAmount= text.length // creo 
-    return CharacterAmount
+    const CharacterCount= text.length 
+    return CharacterCount
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-   // Eliminamos los espacios y signos de puntuación utilizando una expresión regular
-   const cleanedText = text.replace(/[^\w\s]|_/g, '').replace(/\s+/g, '');
-
-   // Contamos los caracteres restantes
-   const characterCount = cleanedText.length;
- 
-   // Retornamos el recuento de caracteres
-   return characterCount;
+    //Eliminamos los espacios y signos de puntuación utilizando una expresión regular
+    const cleanedText = text.replace(/[^\w\s]|_/g, '').replace(/\s+/g, '');
+    const characterCount = cleanedText.length;
+    return characterCount;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: () => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    
-  },
+      },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numberCount = /(\d+(\.\d+)?)/g;
+    const decimalnumberCount = text.match(numberCount)
+    console.log(decimalnumberCount)
+    return decimalnumberCount ? decimalnumberCount.length : 0;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numberSum = " "
   },
   
 };
--- src/index.js
+++ src/index.js
@@ -6,15 +6,28 @@ const textarea = document.querySelector('textarea[placeholder="Comienza a escrib
 const wordCountBadge = document.querySelector('span[data-testing="word-count-badge"]') /* Hago referencia al elemento de HTML, paramanipularlo desde JS*/
 const characterCountBadge = document.querySelector('span[data-testing="character-count-badge"]')
 const characterNoSpacesCountBadge = document.querySelector('span[data-testing="character-no-spaces-count-badge"]')
+const numberCountBadge = document.querySelector('span[data-testing="number-count"]')
 
 //let WordCount = analyzer.getWordCount(textarea.value)
-textarea.addEventListener("input",(e)=>{
-    //console.log(e.target.value) 
-    let words = analyzer.getWordCount(textarea.value)
-    wordCountBadge.textContent= words;
-    let characters = analyzer.getCharacterCount(textarea.value)
-    characterCountBadge.textContent= characters;
-    let characterNoSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value)
-    characterNoSpacesCountBadge.textContent= characterNoSpaces
+textarea.addEventListener("input",()=>{
+  const words = analyzer.getWordCount(textarea.value)
+  wordCountBadge.textContent= words;
+  const characters = analyzer.getCharacterCount(textarea.value)
+  characterCountBadge.textContent= characters;
+  const characterNoSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value)
+  characterNoSpacesCountBadge.textContent= characterNoSpaces
+  const numbers = analyzer.getNumberCount(textarea.value)
+  numberCountBadge.textContent= numbers
 })
 
+const textInput = document.getElementsByName('user-input')[0];
+const resetButton = document.querySelector('#reset-button');
+const list = document.querySelector('span [data-testing]');
+
+resetButton.addEventListener('click', () => {
+  textInput.value = '0';
+  wordCountBadge.textContent= '0';
+  characterCountBadge.textContent= '0';
+  characterNoSpacesCountBadge.textContent= '0';
+  numberCountBadge.textContent= '0';
+});
