cbae11b09f4b9ca9aaaf6e7e2214b0977a6b048b
Linda Reyes <lindamreyesr@gmail.com>
2023-09-06 22:43:39 -0500

    Funcionalidad completada

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 const textarea = document.querySelector('textarea[placeholder="Comienza a escribir un texto aqui..."]');
-const wordCountBadge = document.querySelector('span[data-testing="word-count-badge"]') /* Hago referencia al elemento de HTML, paramanipularlo desde JS*/
-const characterCountBadge = document.querySelector('span[data-testing="character-count-badge"]')
-const characterNoSpacesCountBadge = document.querySelector('span[data-testing="character-no-spaces-count-badge"]')
-const numberCountBadge = document.querySelector('span[data-testing="number-count"]')
-const numberSumBadge = document.querySelector('span[data-testing="number-sum"]')
-const wordLengthAverageBadge = document.querySelector('span[data-testing="word-length-average"]')
+const wordCount = document.querySelector('li[data-testing="word-count"]') /* Hago referencia al elemento de HTML, paramanipularlo desde JS*/
+const characterCount = document.querySelector('li[data-testing="character-count"]')
+const characterNoSpacesCount = document.querySelector('li[data-testing="character-no-spaces-count"]')
+const numberCount = document.querySelector('li[data-testing="number-count"]')
+const numberSumTotal = document.querySelector('li[data-testing="number-sum"]')
+const wordLengthAverage = document.querySelector('li[data-testing="word-length-average"]')
 
 //let WordCount = analyzer.getWordCount(textarea.value)
 textarea.addEventListener("keyup",()=>{
   const words = analyzer.getWordCount(textarea.value)
-  wordCountBadge.textContent= words;
+  wordCount.textContent= words + " Palabras";
   const characters = analyzer.getCharacterCount(textarea.value)
-  characterCountBadge.textContent= characters;
+  characterCount.textContent= characters + " Caracteres (con espacios)";
   const characterNoSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value)
-  characterNoSpacesCountBadge.textContent= characterNoSpaces;
+  characterNoSpacesCount.textContent= characterNoSpaces + " Caracteres (sin espacios ni signos)";
   const numbers = analyzer.getNumberCount(textarea.value)
-  numberCountBadge.textContent= numbers;
+  numberCount.textContent= numbers + " Números";
   const numberSum = analyzer.getNumberSum(textarea.value)
-  numberSumBadge.textContent= numberSum;
+  numberSumTotal.textContent= numberSum + " Suma de números";
   const AverageLength = analyzer.getAverageWordLength(textarea.value)
-  wordLengthAverageBadge.textContent= AverageLength;
+  wordLengthAverage.textContent= AverageLength + " Longitud media de palabras";
 })
 
 const textInput = document.getElementsByName('user-input')[0];
@@ -31,10 +31,10 @@ const resetButton = document.getElementById('reset-button');
 
 resetButton.addEventListener('click', () => {
   textInput.value = '';
-  wordCountBadge.textContent= '0';
-  characterCountBadge.textContent= '0';
-  characterNoSpacesCountBadge.textContent= '0';
-  numberCountBadge.textContent= '0';
-  numberSumBadge.textContent= '0';
-  wordLengthAverageBadge.textContent= '0';
+  wordCount.textContent= '0 Palabras';
+  characterCount.textContent= '0 Caracteres (con espacios)';
+  characterNoSpacesCount.textContent= '0 Caracteres (sin espacios, ni signos)"';
+  numberCount.textContent= '0 Números';
+  numberSumTotal.textContent= '0 Suma de números';
+  wordLengthAverage.textContent= '0 Longitud media de palabras';
 });
